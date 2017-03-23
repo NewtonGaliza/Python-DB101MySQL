4456bd93be172cab09c46ea3e03a080a7601e75f
Newton Galiza <newtonjgaliza@gmail.com>
2017-03-23 15:36:56 -0300

    Create db101.3.py

---
+++ Python-DB101.3/db101.3.py
@@ -0,0 +1,17 @@
+import MySQLdb as mdb
+
+con = mdb.connect('<insert>', '<insert>', '<insert>', '<insert>')
+
+with con:
+
+	cur = con.cursor()
+	cur.execute('SELECT * FROM Writers')
+
+	rows = cur.fetchall()
+
+	desc = cur.description
+
+	print ('%s %3s') % (desc[0][0], desc[1][0])
+
+	for row in rows:
+		print ('%2s %3s') % row
